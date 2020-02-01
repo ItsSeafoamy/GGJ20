e2343f437c20918818c93a18e88f23e1cf875886
Laxynd <laxynd@siblaze.com>
2020-02-01 18:12:11 +0000

    Moving Platform Update

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,6 +7,8 @@ public class Game : MonoBehaviour {
 	public static Game i;
 	public static int[] scores = new int[] {0, 0};
 
+	public GUISkin skin, rightAlign;
+
 	public Color[] colours;
 	public float screenEdge;
 	public float topEdge, bottomEdge;
@@ -17,7 +19,12 @@ public class Game : MonoBehaviour {
 	}
 
 	private void OnGUI() {
+		GUI.skin = skin;
+		GUI.color = colours[0];
 		GUI.Label(new Rect(16, 16, 1000, 1000), "Player 1: " + scores[0]);
-		GUI.Label(new Rect(Screen.width - 100, 16, 1000, 1000), "Player 2: " + scores[1]);
+
+		GUI.skin = rightAlign;
+		GUI.color = colours[1];
+		GUI.Label(new Rect(Screen.width - 216, 16, 200, 1000), "Player 2: " + scores[1]);
 	}
 }
--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour {
 
 	public bool up = false;
-	public float speed;
-	public float high, low;
+	public bool right = false;
+
+	public float vertSpeed, horizSpeed;
+	public float high, low, leftMost, rightMost;
 
 	private void Update() {
 		if (up) {
-			transform.position += new Vector3(0, speed*Time.deltaTime);
+			transform.position += new Vector3(0, vertSpeed*Time.deltaTime);
 
 			if (transform.position.y > high) up = false;
 		} else {
-			transform.position -= new Vector3(0, speed*Time.deltaTime);
+			transform.position -= new Vector3(0, vertSpeed*Time.deltaTime);
 
 			if (transform.position.y < low) up = true;
+		}
+
+		if (right) {
+			transform.position += new Vector3(horizSpeed*Time.deltaTime, 0);
+
+			if (transform.position.x > rightMost) right = false;
+		} else {
+			transform.position -= new Vector3(horizSpeed*Time.deltaTime, 0);
+
+			if (transform.position.x < leftMost) right = true;
 		}
 	}
 }
